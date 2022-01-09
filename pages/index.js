4441277910de5b3ea31239aa767f369ef59cbd29
Daniel Kim <kkdh1109@gmail.com>
2022-01-09 18:22:46 -0500

    fix urls, and fetch data using serversideprops function

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 import styles from "../styles/Home.module.css";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
+  console.log(results);
+
   return (
     <div>
       <Head>
@@ -16,6 +20,25 @@ export default function Home() {
       <Header />
 
       <Nav />
+
+      <Results results={results} />
     </div>
   );
+}
+
+//server side rendering using query
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
