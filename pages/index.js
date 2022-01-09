bcc4e7476704972e8a045f1786ca0f44ee6e3a13
Daniel Kim <kkdh1109@gmail.com>
2022-01-09 14:43:49 -0500

    create a request file to store fetch info including urls of tmdb api

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -13,6 +14,8 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <Nav />
     </div>
   );
 }
